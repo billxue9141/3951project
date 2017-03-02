7b0db2e59902b81c43224ca6f7addaba94c3fdb5
billxue9141 <billxue9141@hotmail.com>
2017-03-02 11:10:20 -0800

    l

---
+++ fireDefenderGame/GameBoard.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace fireDefenderGame
+{
+    class GameBoard
+    {
+        private Tile[ , ] board;
+        private int row;
+        private int col;
+
+        public GameBoard(int row, int col)
+        {
+            this.row = row;
+            this.col = col;
+            board = new Tile[row,col];
+
+        }
+    }
+}
